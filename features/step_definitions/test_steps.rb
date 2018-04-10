e0e3fa2cbed4687a488ffa59d08336acfb93c6a6
Jack Long <jacklong101@tamu.edu>
2018-04-10 11:43:39 +0000

    Headless

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -4,7 +4,7 @@ require 'watir-webdriver'
 
 $DEBUG = true
 #driver = Selenium::WebDriver.for :chrome
-driver = Watir::Browser.new :phantomjs
+driver = Watir::Browser.new :firefox
 
 When ("I go to google") do 
     #driver.get "http://google.com"
