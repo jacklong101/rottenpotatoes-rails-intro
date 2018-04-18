4ae45f2b95ebf2cb7fc24df0b1d11262442922eb
Jack Long <jacklong101@tamu.edu>
2018-04-18 11:38:51 +0000

    Test

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -3,15 +3,9 @@ require 'selenium-webdriver'
 require 'watir-webdriver'
 require 'cucumber'
 
-caps = {
-    :browser_name          => 'chrome',
-    :javascript_enabled    => true,
-    :takes_screenshot      => true
-    }
-
 #$DEBUG = true
-#driver = Selenium::WebDriver.for :chrome
-driver = Selenium::WebDriver.for(:remote, :url => "http://127.0.0.1:4444/wd/hub", :desired_capabilities => caps)
+driver = Selenium::WebDriver.for :chrome
+#driver = Selenium::WebDriver.for(:remote, :url => "http://127.0.0.1:4444/wd/hub")
 #driver = Watir::Browser.new :firefox
 
 When ("I go to google") do 
